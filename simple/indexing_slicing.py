278611c237621392201aff0efc2a6b0e32e1d033
fhsi1 <eugenie8@kakao.com>
2021-02-02 18:34:39 +0900

    simple

---
+++ simple/indexing_slicing.py
@@ -0,0 +1,7 @@
+temp = "goorm can make rainbow with best environment"
+
+over = temp[1] + temp[-9] + temp[-11] + temp[3]
+the = temp[-1] + temp[-18] + temp[-11]
+rainbow = temp[15:22]
+
+print(over, the, rainbow)
---
+++ simple/string_formating.py
@@ -0,0 +1,6 @@
+year = int(input())
+month = int(input())
+today = int(input())
+day = input()
+
+print("오늘은 %d년 %d월 %d일 %s입니다." %(year, month, today, day))
